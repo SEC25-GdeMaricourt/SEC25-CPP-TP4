cc4c05768e8f35ba687b059aed8167888e3ce8c8
Guillaume de Maricourt <ahz45bny@gmail.com>
2024-01-28 18:59:28 +0100

    Add main

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#ifdef TEST
+#include "tests/tests.hpp"
+#endif
+
+using namespace std;
+
+/*
+Set this flag when compiling ("-D TEST")
+to trigger the "test mode display"
+*/
+
+int main(void)
+{
+#ifdef TEST
+    testPoint();
+    cout << endl;
+    testPolygone();
+    cout << endl;
+    // testParcelle();
+    testZN();
+#else
+
+#endif
+    return 0;
+}
